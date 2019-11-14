d662f8f2d856c2ef0c0d9d3f0d927d3be322d3ec
CloseGuan <closeguan@smail.nchu.edu.tw>
2019-11-14 21:54:48 +0800

    2_2019.11.09 class progress

---
+++ mainsite/views.py
@@ -0,0 +1,13 @@
+from django.template.loader import get_template
+from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
+# Create your views here.
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+
