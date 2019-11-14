f2c6ac6838067dd6677d497810a279a9dc80827b
CloseGuan <closeguan@smail.nchu.edu.tw>
2019-11-14 22:00:56 +0800

    4_myblog-header_footer-20191110-part1

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,7 +1,10 @@
 from django.template.loader import get_template
 from django.http import HttpResponse
+from django.shortcuts import redirect  #  
 from datetime import datetime
 from .models import Post
+
+
 # Create your views here.
 def homepage(request):
     template = get_template('index.html')
@@ -9,5 +12,16 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
+
 
 
