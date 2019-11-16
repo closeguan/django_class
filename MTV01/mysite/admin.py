2aef076ff33bbd613ca8395af5b14fef5d9676b3
CloseGuan <closeguan@smail.nchu.edu.tw>
2019-11-17 00:34:17 +0800

    2019-11-16 晚上練習_hw1_N_new-table_and_product

--- MTV01/mysite/admin.py
+++ MTV01/mysite/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Product
-from mysite.models import NewTable, Product 
+from .models import Product ,NewTable
+from mysite.models import Product, NewTable 
 #admin.site.register(NewTable)
 #admin.site.register(Product)
 
@@ -10,4 +10,10 @@ from mysite.models import NewTable, Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','price','qty')
 
+
+
+class NewTableAdmin(admin.ModelAdmin):
+    list_display = ('char_f','date_f','datetime_f')
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(NewTable, NewTableAdmin)
